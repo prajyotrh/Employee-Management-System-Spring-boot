793e410af62fbc42d988517df2694a01c79a6070
prajyotrh <52719027+prajyotrh@users.noreply.github.com>
2021-10-30 12:00:54 +0530

    Update EmployeeService.java

--- src/main/java/com/techtalk/spring_crud_app/services/EmployeeService.java
+++ src/main/java/com/techtalk/spring_crud_app/services/EmployeeService.java
@@ -2,10 +2,12 @@ package com.techtalk.spring_crud_app.services;
 
 import com.techtalk.spring_crud_app.model.Employee;
 import com.techtalk.spring_crud_app.repository.EmployeeRepository;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 public class EmployeeService {
@@ -31,12 +33,15 @@ public class EmployeeService {
 
     public Employee updateEmployee(Employee employee) {
         Employee existingEMP = employeeRepository.findById(employee.getId()).orElse(null);
-
+        System.out.println(employee);
         if(existingEMP == null) {
+            System.out.println("Emp not found");
             return  employeeRepository.save(employee);
         }else  {
-            employeeRepository.deleteById(existingEMP.getId());
-            employeeRepository.save(employee);
+            existingEMP.setName(employee.getName());
+            existingEMP.setEmail(employee.getEmail());
+            existingEMP.setSalary(employee.getSalary());
+            employeeRepository.save(existingEMP);
         }
         return employee;
     }
@@ -48,5 +53,9 @@ public class EmployeeService {
             return true;
         }
         return false;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
     }
 }
